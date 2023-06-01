221a6deb8070813d1c41e0cce5f31a901435ccac
unknown <ayoub.sguiar-lhamdani@student-cs.fr>
2023-06-01 14:49:38 +0200

    Search bar is working, connected deirectly to the API

--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -1,20 +1,55 @@
-import React from "react"
-import "./Header.css"
-import { Link } from "react-router-dom"
+import React, { useState } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+import './Header.css';
 
 const Header = () => {
+  const [searchValue, setSearchValue] = useState('');
+  const navigate = useNavigate();
+
+  const handleSearchChange = (event) => {
+    setSearchValue(event.target.value);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    navigate(`/search?q=${searchValue}`);
+  };
+
   return (
     <div className="header">
       <div className="headerLeft">
-        <Link to="/"><img className="header__icon" src="https://upload.wikimedia.org/wikipedia/commons/thumb/6/69/IMDB_Logo_2016.svg/2560px-IMDB_Logo_2016.svg.png" /></Link>
-        <Link to="/movies/popular" style={{ textDecoration: "none" }}><span>Popular</span></Link>
-        <Link to="/movies/top_rated" style={{ textDecoration: "none" }}><span>Top Rated</span></Link>
-        <Link to="/movies/upcoming" style={{ textDecoration: "none" }}><span>Upcoming</span></Link>
-        <Link to="/users" style={{ textDecoration: "none" }}><span>Sign in</span></Link>
-        <Link to="/about" style={{ textDecoration: "none" }}><span>About</span></Link>
+        <Link to="/">
+          <img className="header__icon" src="./csshows.png" alt="Logo" />
+        </Link>
+        <Link to="/movies/popular" style={{ textDecoration: 'none' }}>
+          <span>Popular</span>
+        </Link>
+        <Link to="/movies/top_rated" style={{ textDecoration: 'none' }}>
+          <span>Top Rated</span>
+        </Link>
+        <Link to="/movies/upcoming" style={{ textDecoration: 'none' }}>
+          <span>Upcoming</span>
+        </Link>
+        <Link to="/users" style={{ textDecoration: 'none' }}>
+          <span>Sign in</span>
+        </Link>
+        <Link to="/about" style={{ textDecoration: 'none' }}>
+          <span>About</span>
+        </Link>
+      </div>
+      <div className="headerRight">
+        <form onSubmit={handleSubmit}>
+          <input
+            type="text"
+            placeholder="Search movies..."
+            value={searchValue}
+            onChange={handleSearchChange}
+          />
+          <button type="submit">Search</button>
+        </form>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
---
+++ frontend/src/components/SearchResults/SearchResults.jsx
@@ -0,0 +1,47 @@
+import React, { useEffect, useState } from 'react';
+import { useLocation } from 'react-router-dom';
+import axios from 'axios';
+
+const SearchResults = () => {
+  const location = useLocation();
+  const searchQuery = new URLSearchParams(location.search).get('q');
+  const [searchResults, setSearchResults] = useState([]);
+
+  useEffect(() => {
+    const options = {
+      method: 'GET',
+      url: 'https://api.themoviedb.org/3/search/movie',
+      params: {
+        query: searchQuery,
+        include_adult: 'false',
+        language: 'en-US',
+        page: '1',
+      },
+      headers: {
+        accept: 'application/json',
+        Authorization:
+          'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIxZjlmNjAwMzY4MzMzODNkNGIwYjNhNzJiODA3MzdjNCIsInN1YiI6IjY0NzA5YmE4YzVhZGE1MDBkZWU2ZTMxMiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.Em7Y9fSW94J91rbuKFjDWxmpWaQzTitxRKNdQ5Lh2Eo',
+      },
+    };
+
+    axios
+      .request(options)
+      .then(function (response) {
+        setSearchResults(response.data.results);
+      })
+      .catch(function (error) {
+        console.error(error);
+      });
+  }, [searchQuery]);
+
+  return (
+    <div>
+      <h2>Search Results for "{searchQuery}"</h2>
+      {searchResults.map((result) => (
+        <div key={result.id}>{result.title}</div>
+      ))}
+    </div>
+  );
+};
+
+export default SearchResults;
