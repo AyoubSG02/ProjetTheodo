b71275b69fce6a89d4345583c30768db17b3444e
unknown <ayoub.sguiar-lhamdani@student-cs.fr>
2023-06-02 11:08:38 +0200

    Database ready and function populate in progress

--- backend/entities/movies.js
+++ backend/entities/movies.js
@@ -3,16 +3,49 @@ import typeorm from 'typeorm';
 const Movie = new typeorm.EntitySchema({
   name: 'Movie',
   columns: {
-    id: {
+    idfilm: {
       primary: true,
       type: Number,
       generated: true,
     },
-    title: {
+    adult: { type: Boolean },
+    backdrop_path: {
       type: String,
+    },
+    genre_ids: {
+      type: 'simple-array',
+      nullable: true,
+    },
+    id: {
+      type: Number,
       unique: true,
     },
-    releaseDate: { type: Date },
+    original_language: {
+      type: String,
+    },
+    original_title: {
+      type: String,
+    },
+    overview: {
+      type: String,
+    },
+    popularity: {
+      type: Number,
+    },
+    poster_path: {
+      type: String,
+    },
+    release_date: { type: Date },
+    title: {
+      type: String,
+    },
+    video: { type: Boolean },
+    vote_average: {
+      type: Number,
+    },
+    vote_count: {
+      type: Number,
+    },
     liked: { type: Boolean, default: false },
   },
 });
--- backend/routes/movies.js
+++ backend/routes/movies.js
@@ -1,39 +1,98 @@
 import express from 'express';
+import axios from 'axios';
 import Movie from '../entities/movies.js';
 import { appDataSource } from '../datasource.js';
+
 //const express = require('express');
 const router = express.Router();
 const movieRepository = appDataSource.getRepository(Movie);
+const options = {
+  method: 'GET',
+  headers: {
+    accept: 'application/json',
+    Authorization:
+      'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIwYjNlNzg0ODkzMDUxMjRjYmQ3YjNiMmViZjMyZjNjNCIsInN1YiI6IjY0NzBhYjRhNzcwNzAwMDExOTI0OGZlYSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.-XX-u9jsBzlN_VSkOYDNyk11_AGkIqX1b3H1XK0_1YE',
+  },
+};
+
 router.get('/', async (req, res) => {
   const movies = await movieRepository.find();
   res.json(movies);
 });
+
 // Écouteur sur le chemin /
 router.get('/', (req, res) => {
   console.log("L'utilisateur a accédé à l'URL /");
   res.send("Bienvenue sur la page d'accueil");
 });
 
+// Populate movies into the database
+router.get('/populate', async (req, res) => {
+  try {
+    const movielist = [];
+
+    for (let i = 1; i <= 50; i++) {
+      movielist.push(
+        axios.get(
+          `https://api.themoviedb.org/3/movie/popular?language=en-US&page=${i}`,
+          options
+        )
+      );
+    }
+
+    const responses = await Promise.all(movielist);
+    const moviesData = responses.flatMap((response) => response.data.results);
+
+    for (const movieData of moviesData) {
+      const movie = movieRepository.create(movieData);
+      await movieRepository.save(movie);
+    }
+
+    res.send('Movies populated successfully!');
+  } catch (error) {
+    res.status(500).send('Failed to populate movies.');
+  }
+});
+
 // Définir la route POST /new
 router.post('/new', async (req, res) => {
   // Récupérer les données du corps de la requête
-  const { title, releaseDate } = req.body;
+  const {
+    title,
+    adult,
+    video,
+    id,
+    genre_ids,
+    backdrop_path,
+    release_date,
+    liked,
+    original_language,
+    original_title,
+    overview,
+    popularity,
+    poster_path,
+    vote_average,
+    vote_count,
+  } = req.body;
 
   // Créer une instance de l'entité Movie
-  const movie = movieRepository.create({ title, releaseDate });
-
-  // Sauvegarder le film dans la base de données
-  await movieRepository.insert(movie);
-
-  res.send('Film ajouté avec succès !');
-});
-
-router.post('/movies/like/:id', async (req, res) => {
-  // Récupérer les données du corps de la requête
-  const { title, releaseDate } = req.body;
-
-  // Créer une instance de l'entité Movie
-  const movie = movieRepository.create({ title, releaseDate });
+  const movie = movieRepository.create({
+    title,
+    adult,
+    video,
+    id,
+    genre_ids,
+    backdrop_path,
+    release_date,
+    liked,
+    original_language,
+    original_title,
+    overview,
+    popularity,
+    poster_path,
+    vote_average,
+    vote_count,
+  });
 
   // Sauvegarder le film dans la base de données
   await movieRepository.insert(movie);
@@ -47,7 +106,9 @@ router.delete('/:id', async (req, res) => {
     await movieRepository.delete(id);
     res.send('Film supprimé avec succès !');
   } catch (error) {
-    res.status(500).send('Une erreur s est produite lors de la suppression du film');
+    res
+      .status(500)
+      .send('Une erreur s est produite lors de la suppression du film');
   }
 });
 
